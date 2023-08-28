d54683f786c22384e88523809b2104b83c7a17cf
Mathieu Mescam <mate@e.email>
2023-08-28 10:57:02 +0200

    bookmarks 15 days old status

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from dotenv import load_dotenv
 import os
-import requests
 import json
+import requests
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,60 +11,26 @@ load_dotenv()
 instance_url = os.getenv('MASTODON_INSTANCE_URL')
 access_token = os.getenv('MASTODON_ACCESS_TOKEN')
 
-# Initialize variables
-all_bookmarks = []
-next_max_id = None
+headers = {
+    'Authorization': f'Bearer {access_token}'
+}
 
-while True:
-    # Construct the API endpoint URL
-    endpoint = f'{instance_url}/api/v1/bookmarks'
+# Load bookmarks.json
+with open('bookmarks.json', 'r') as file:
+    bookmarks_data = json.load(file)
 
-    # Set up headers with the access token
-    headers = {'Authorization': f'Bearer {access_token}'}
+# Calculate the date 15 days ago
+fifteen_days_ago = datetime.now() - timedelta(days=15)
 
-    # Set up parameters for pagination
-    params = {'max_id': next_max_id} if next_max_id else {}
+# Loop through statuses in bookmarks_data and boost if they are 15 days old
+for status in bookmarks_data:
+    status_date = datetime.strptime(status['created_at'], '%Y-%m-%d')
+    if status_date <= fifteen_days_ago:
+        status_id = status['status_id']
+        boost_url = f'{instance_url}/api/v1/statuses/{status_id}/reblog'
+        response = requests.post(boost_url, headers=headers)
 
-    # Make the API request
-    response = requests.get(endpoint, headers=headers, params=params)
-
-    if response.status_code == 200:
-        bookmarks = response.json()
-
-        # Append the current page of bookmarks to the list
-        all_bookmarks.extend(bookmarks)
-
-        # Check if there are more pages
-        if 'Link' in response.headers:
-            link_header = response.headers['Link']
-            if 'rel="next"' in link_header:
-                next_max_id = link_header.split('max_id=')[1].split('&')[0]
-            else:
-                break
+        if response.status_code == 200:
+            print(f"Boosted status with ID {status_id}")
         else:
-            break
-    else:
-        print(f"Error fetching bookmarks. Status code: {response.status_code}")
-        break
-
-# Now all_bookmarks contains all the bookmarked statuses
-print("Total bookmarked statuses:", len(all_bookmarks))
-
-data = []
-
-for x in range(0, len(all_bookmarks)-1):
-    print(all_bookmarks[x]['created_at'], all_bookmarks[x]['url'])
-
-    data.append({
-        "created_at": all_bookmarks[x]['created_at'].format().split("T")[0],
-        "url": all_bookmarks[x]['url']
-    })
-
-    # Specify the file name
-    filename = "bookmarks.json"
-
-    # Write the data to a JSON file
-    with open(filename, "w") as json_file:
-        json.dump(data, json_file)
-        
-print(f"JSON file '{filename}' created successfully.")
+            print(f"Error boosting status with ID {status_id}: {response.text}")
